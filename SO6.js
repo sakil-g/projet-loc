76755ad969d56dd8d19c7587215547940d8fd7da
Simplon <josepjoestar224@gmail.com>
2020-10-26 10:35:39 +0400

    ajout carousel

---
+++ SO6.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+
+
+    $('.items').slick({
+    infinite: true,
+    lazyLoad: 'ondemand',
+    slidesToShow: 5,
+    slidesToScroll: 3
+    });
+    
+    
+    
+    
+    
+    
+    });
